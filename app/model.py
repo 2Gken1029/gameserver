c67f136f2ac0d860f6863e0b97f983698070a0df
Kenichiro Tsuji <70386620+2Gken1029@users.noreply.github.com>
2022-03-11 02:46:19 +0000

    commit

--- app/model.py
+++ app/model.py
@@ -64,6 +64,8 @@ def update_user(token: str, name: str, leader_card_id: int) -> None:
     with engine.begin() as conn:
         # TODO: 実装
         conn.execute(
-            text("UPDATE `user` SET name=:name, leader_card_id =:leader_card_id WHERE `token`=:token"),
+            text(
+                "UPDATE `user` SET name=:name, leader_card_id =:leader_card_id WHERE `token`=:token"
+            ),
             {"name": name, "token": token, "leader_card_id": leader_card_id},
         )
